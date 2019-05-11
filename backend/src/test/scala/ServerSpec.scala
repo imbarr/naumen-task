649ebd7fb6cc3f27cccba124a72ad68ad488d4fb
imbar <imbrkvskh@gmail.com>
2019-05-11 14:41:54 +0500

    rewrite Server tests using mock

--- backend/src/test/scala/ServerSpec.scala
+++ backend/src/test/scala/ServerSpec.scala
@@ -1,83 +1,93 @@
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest, StatusCodes}
+import akka.http.scaladsl.model.Uri.Query
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.typesafe.scalalogging.Logger
-import data.{BookEntry, BookEntryWithId, NameWrapper, PhoneNumberWrapper}
-import org.scalatest.{Assertion, BeforeAndAfter, FlatSpec}
-import storage.InMemoryBook
 import data.Implicits._
+import data.{BookEntry, BookEntryWithId, NameWrapper, PhoneNumberWrapper}
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{Assertion, BeforeAndAfter, FlatSpec}
+import storage.Book
 import util.CirceMarshalling._
 
-class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter {
+class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter with MockFactory {
   implicit val log = Logger("server-spec")
 
-  val entries = List(
-    BookEntry("John Doe", "88005553535"),
-    BookEntry("Jane Doe", "+79223101010"),
-    BookEntry("Jane Doe", "+79223101010"),
-    BookEntry("Someone Else", "dfgdgdgdfgd")
-  )
-
+  var book: Book = _
   var server: Server = _
-  var added: List[BookEntryWithId] = _
 
   before {
-    val book = new InMemoryBook()
-    added = entries.map(entry => {
-      val id = book.add(entry)
-      BookEntryWithId(id, entry.name, entry.phoneNumber)
-    })
+    book = mock[Book]
     server = new Server(book)
   }
 
-  "Server" should "return all phonebook entries" in
+  "Server" should "return all phonebook entries" in {
+    val entries = List(
+      BookEntryWithId(22, "John", "343453"),
+      BookEntryWithId(11, "Boris", "3354354")
+    )
+    book.getAll _ expects() returning entries
     Get("/phonebook") ~> server.routes ~> check {
       assert(status == StatusCodes.OK)
-
       val result = responseAs[List[BookEntryWithId]]
-      val withoutIds = result.map(entry => BookEntry(entry.name, entry.phoneNumber))
-      assert(withoutIds == entries)
+      assert(result == entries)
     }
+  }
 
-  it should "create new phonebook entry" in
-    Post("/phonebook", BookEntry("New", "223424234")) ~> server.routes ~> check {
+  it should "create new phonebook entry" in {
+    val entry = BookEntry("Bob", "dgfdgfdf")
+    val id = 122
+    book.add _ expects entry returning id
+    Post("/phonebook", entry) ~> server.routes ~> check {
       assert(status == StatusCodes.Created)
-      assert(header("Location").isDefined)
+      assert(header("Location").contains(Location(s"/phonebook/$id")))
     }
+  }
 
   it should "find entries by name substring" in {
-    Get("/phonebook?nameSubstring=Doe") ~> server.routes ~> check {
+    val substring = "Doe"
+    val foundEntries = List(
+      BookEntryWithId(11, "John Doe", "456456"),
+      BookEntryWithId(22, "Jane Doe", "232323")
+    )
+    book.findByNameSubstring _ expects substring returning foundEntries
+
+    val uri = Uri("/phonebook").withQuery(Query("nameSubstring" -> substring))
+    Get(uri) ~> server.routes ~> check {
       assert(status == StatusCodes.OK)
       val result = responseAs[List[BookEntryWithId]]
-      assert(result.length == 3)
-    }
-    Get("/phonebook?nameSubstring=John") ~> server.routes ~> check {
-      assert(status == StatusCodes.OK)
-      val result = responseAs[List[BookEntryWithId]]
-      assert(result.length == 1)
+      assert(result == foundEntries)
     }
   }
 
   it should "find entries by telephone substring" in {
-    Get("/phonebook?phoneSubstring=%2B7") ~> server.routes ~> check {
-      assert(status == StatusCodes.OK)
-      val result = responseAs[List[BookEntryWithId]]
-      assert(result.length == 2)
-    }
+    val substring = "+7"
+    val foundEntries = List(
+      BookEntryWithId(11, "John Doe", "+7456456"),
+      BookEntryWithId(22, "Jane Doe", "+7232323")
+    )
+    book.findByPhoneNumberSubstring _ expects substring returning foundEntries
 
-    Get("/phonebook?phoneSubstring=") ~> server.routes ~> check {
+    val uri = Uri("/phonebook").withQuery(Query("phoneSubstring" -> substring))
+    Get(uri) ~> server.routes ~> check {
       assert(status == StatusCodes.OK)
       val result = responseAs[List[BookEntryWithId]]
-      assert(result.length == entries.length)
+      assert(result == foundEntries)
     }
   }
 
   it should "update entry" in {
-    val id = added.head.id
+    val id = 11
+    val name = "name"
+    val phone = "8995235523"
+    book.replace _ expects(id, name, phone) returning true noMoreThanOnce()
+    book.changeName _ expects(id, name) returning true noMoreThanOnce()
+    book.changePhoneNumber _ expects(id, phone) returning true noMoreThanOnce()
     val requests = List(
-      Patch("/phonebook/" + id, BookEntry("New name", "New phone")),
-      Patch("/phonebook/" + id, NameWrapper("New name")),
-      Patch("/phonebook/" + id, PhoneNumberWrapper("New name"))
+      Patch(s"/phonebook/$id", BookEntry(name, phone)),
+      Patch(s"/phonebook/$id", NameWrapper(name)),
+      Patch(s"/phonebook/$id", PhoneNumberWrapper(phone))
     )
 
     testForAll(requests) {
@@ -86,17 +96,23 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter {
   }
 
   it should "delete entry" in {
-    val id = added.head.id
-    Delete("/phonebook/" + id) ~> server.routes ~> check {
+    val id = 11
+    book.remove _ expects id returning true
+    Delete(s"/phonebook/$id") ~> server.routes ~> check {
       assert(status == StatusCodes.OK)
     }
   }
 
   it should "return 404 for non-existing resources" in {
-    val id = added.map(_.id).max + 1
+    val id = 20
+    val name = "name"
+    val phone = "345353"
+    book.remove _ expects id returning false anyNumberOfTimes()
+    book.replace _ expects(id, name, phone) returning false anyNumberOfTimes()
     val requests = List(
       Get("/something"),
-      Delete("/phonebook/" + id)
+      Delete(s"/phonebook/$id"),
+      Patch(s"/phonebook/$id", BookEntry(name, phone))
     )
 
     testForAll(requests) {
@@ -105,12 +121,12 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter {
   }
 
   it should "return 405 for not allowed methods" in {
-    val id = added.head.id
+    val id = 22
     val requests = List(
       Delete("/phonebook"),
       Patch("/phonebook"),
-      Get("/phonebook/" + id),
-      Put("/phonebook/" + id)
+      Get(s"/phonebook/$id"),
+      Put(s"/phonebook/$id")
     )
 
     testForAll(requests) {
@@ -119,12 +135,12 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter {
   }
 
   it should "return 400 for incorrect requests" in {
-    val id = added.head.id
+    val id = 22
     val requests = List(
       Post("/phonebook", HttpEntity(ContentTypes.`application/json`, "")),
       Post("/phonebook", NameWrapper("name")),
-      Patch("/phonebook/" + id, HttpEntity(ContentTypes.`application/json`, "{\"malformed\": json}")),
-      Patch("/phonebook/" + id, "{\"some\": \"json\"}")
+      Patch(s"/phonebook/$id", HttpEntity(ContentTypes.`application/json`, "{\"malformed\": json}")),
+      Patch(s"/phonebook/$id", "{\"some\": \"json\"}")
     )
 
     testForAll(requests) {
