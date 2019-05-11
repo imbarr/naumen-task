9ff2adb2fcb935b88757cfb40461b6d4cdf1895a
imbar <imbrkvskh@gmail.com>
2019-05-11 15:24:15 +0500

    add quitting option

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -1,12 +1,17 @@
 import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.Logger
 import config.Config
 import storage.InMemoryBook
 import pureconfig.generic.auto._
 
+import scala.io.StdIn
+
 object Main extends App {
   implicit val log = Logger("naumen-task")
   implicit val system = ActorSystem("naumen-task")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = materializer.executionContext
 
   pureconfig.loadConfig[Config] match {
     case Left(_) =>
@@ -15,7 +20,11 @@ object Main extends App {
     case Right(config) =>
       val book = new InMemoryBook()
       log.info("Starting server...")
-      new Server(book).start(config.server)
-      log.info(s"Server is up on ${config.server.interface}:${config.server.port}")
+      val bindingFuture = new Server(book).start(config.server)
+      log.info(s"Server is up on ${config.server.interface}:${config.server.port}. Press ENTER to quit.")
+      bindingFuture.flatMap(binding => {
+        StdIn.readLine()
+        binding.unbind()
+      }).onComplete(_ => system.terminate())
   }
 }
