9a55a5709f6249c2125def440ad0c7c4205743d5
imbar <imbrkvskh@gmail.com>
2019-05-13 20:35:43 +0500

    make credentials for migration optional

--- backend/src/main/scala/config/Config.scala
+++ backend/src/main/scala/config/Config.scala
@@ -4,6 +4,6 @@ case class Config(server: ServerConfig, database: DatabaseConfig)
 
 case class ServerConfig(interface: String, port: Int)
 
-case class DatabaseConfig(url: String, backend: Credentials, migration: Credentials)
+case class DatabaseConfig(url: String, backend: Credentials, migration: Option[Credentials])
 
 case class Credentials(user: String, password: String)
