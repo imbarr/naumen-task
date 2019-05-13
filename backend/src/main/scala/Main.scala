f0e29131d81d47963f5b2b737e1ff66fcfef7e64
imbar <imbrkvskh@gmail.com>
2019-05-13 20:37:04 +0500

    add migrations on app startup

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -1,9 +1,9 @@
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.Logger
-import config.Config
-import storage.InMemoryBook
+import config.{Config, DatabaseConfig}
 import pureconfig.generic.auto._
+import storage.InMemoryBook
 
 import scala.io.StdIn
 
@@ -13,11 +13,13 @@ object Main extends App {
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = materializer.executionContext
 
+
   pureconfig.loadConfig[Config] match {
     case Left(_) =>
       log.error("FATAL: failed to load configuration")
       System.exit(1)
     case Right(config) =>
+      migrate(config.database)
       val book = new InMemoryBook()
       log.info("Starting server...")
       val bindingFuture = new Server(book).start(config.server)
@@ -26,5 +28,19 @@ object Main extends App {
         StdIn.readLine()
         binding.unbind()
       }).onComplete(_ => system.terminate())
+  }
+
+  def migrate(config: DatabaseConfig): Unit = {
+    config.migration match {
+      case None =>
+        log.warn("Credentials for database migrations were not specified. Migrations will not run")
+      case Some(credentials) =>
+        new Migration(config.url, credentials).migrate() match {
+          case Some(error) =>
+            log.error("Error occurred while performing migrations", error)
+          case None =>
+            log.info("Database migration successful")
+        }
+    }
   }
 }
