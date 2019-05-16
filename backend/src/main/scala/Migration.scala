8be9daef9b2541d3fab631253b95843361b1d3b0
imbar <imbrkvskh@gmail.com>
2019-05-16 13:23:38 +0500

    move migration code to separate class

--- backend/src/main/scala/Migration.scala
+++ backend/src/main/scala/Migration.scala
@@ -1,16 +1,30 @@
-import config.Credentials
+import com.typesafe.scalalogging.Logger
+import config.DatabaseConfig
 import org.flywaydb.core.Flyway
-import org.flywaydb.core.api.FlywayException
 
-class Migration(url: String, credentials: Credentials) {
-  val flyway = Flyway.configure().dataSource(url, credentials.user, credentials.password).load()
+import scala.util.{Failure, Success, Try}
 
-  def migrate(): Option[FlywayException] =
-    try {
-      flyway.migrate()
-      None
-    } catch {
-      case exception: FlywayException =>
-        Some(exception)
+object Migration {
+  def migrate(config: DatabaseConfig)(implicit log: Logger): Unit = {
+    config.migration match {
+      case None =>
+        log.warn("Credentials for database migrations were not specified. Migrations will not run")
+      case Some(credentials) =>
+        val flyway = Flyway.configure().dataSource(config.databaseUrl, credentials.user, credentials.password).load()
+        putPlaceholders(flyway, config)
+        Try(flyway.migrate()) match {
+          case Failure(error) =>
+            log.error("Error occurred while performing migrations", error)
+          case Success(_) =>
+            log.info("Database migration successful")
+        }
     }
+  }
+
+  private def putPlaceholders(flyway: Flyway, config: DatabaseConfig): Unit = {
+    val placeholders = flyway.getConfiguration.getPlaceholders
+    placeholders.put("user", config.backend.user)
+    placeholders.put("password", config.backend.password)
+    placeholders.put("database", config.database)
+  }
 }
