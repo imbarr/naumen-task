e35129e2430597d62a34b95dd851e55c1fc5a8c5
imbar <imbrkvskh@gmail.com>
2019-05-16 13:20:02 +0500

    change application.conf format

--- backend/src/main/scala/config/Config.scala
+++ backend/src/main/scala/config/Config.scala
@@ -4,6 +4,8 @@ case class Config(server: ServerConfig, database: DatabaseConfig)
 
 case class ServerConfig(interface: String, port: Int)
 
-case class DatabaseConfig(url: String, backend: Credentials, migration: Option[Credentials])
+case class DatabaseConfig(url: String, database: String, backend: Credentials, migration: Option[Credentials]) {
+  val databaseUrl = s"$url;databaseName=$database"
+}
 
 case class Credentials(user: String, password: String)
