cdeb7985c15482d150817a47f01445dab0a46b1a
imbar <imbrkvskh@gmail.com>
2019-05-19 16:15:00 +0500

    remove password policy check on login creation

--- backend/src/main/resources/db/migration/V1__Create_login_and_user.sql
+++ backend/src/main/resources/db/migration/V1__Create_login_and_user.sql
@@ -1,2 +1,2 @@
-create login ${user} with password='${password}', default_database=${database};
+create login ${user} with password='${password}', default_database=${database}, check_policy=off;
 create user ${user} for login ${user};
