35101807d2b62fd40ed38144cfc19ca5132cd3c3
imbar <imbrkvskh@gmail.com>
2019-05-20 15:29:38 +0500

    add DataSaver class for saving contents to disk

---
+++ backend/src/main/scala/DataSaver.scala
@@ -0,0 +1,33 @@
+import java.nio.file.{Files, Path}
+
+import io.circe.Encoder
+import io.circe.syntax.EncoderOps
+
+import scala.collection.JavaConverters.asScalaIteratorConverter
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+
+class DataSaver(path: Path)(implicit executionContext: ExecutionContext) {
+  def save[A: Encoder](name: String, data: A): Future[Unit] = Future {
+    val next = getNextNumber(name)
+    val newFilePath = path.resolve(s"${name}_$next.json")
+    val writer = Files.newBufferedWriter(newFilePath)
+    writer.write(data.asJson.spaces2)
+    writer.close()
+  }
+
+  private def getNextNumber(prefix: String): Int = {
+    val files = Files.list(path).iterator().asScala
+    val filenames = files.map(_.getFileName.toString)
+    val numbers = filenames.flatMap(filename => getNumber(filename, prefix))
+    numbers.fold(0)(_ max _) + 1
+  }
+
+  private def getNumber(filename: String, prefix: String): Option[Int] = {
+    val regex = s"""^${prefix}_(.*).json""".r
+    filename match {
+      case regex(number) => Try(number.toInt).toOption
+      case _ => None
+    }
+  }
+}
---
+++ backend/src/test/scala/DataSaverSpec.scala
@@ -0,0 +1,67 @@
+import java.nio.file.{FileSystem, Files, Path}
+
+import com.google.common.jimfs.{Configuration, Jimfs}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.parser.parse
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+
+import scala.collection.JavaConverters.asScalaIteratorConverter
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+class DataSaverSpec extends FlatSpec with BeforeAndAfter {
+  implicit val executionContext = ExecutionContext.global
+
+  case class Class(string: String, int: Int, boolean: Boolean)
+
+  implicit val encoder = deriveEncoder[Class]
+  implicit val decoder = deriveDecoder[Class]
+
+  var fileSystem: FileSystem = _
+  var directory: Path = _
+  var dataSaver: DataSaver = _
+
+  before {
+    fileSystem = Jimfs.newFileSystem(Configuration.forCurrentPlatform)
+    directory = fileSystem.getRootDirectories.iterator().next().resolve("work")
+    dataSaver = new DataSaver(directory)
+  }
+
+  "DataSaver" should "save file with correct name" in {
+    val data = Class("строка", 0, boolean = false)
+    await(dataSaver.save("something", data))
+    val files = Files.list(directory).iterator().asScala.toList
+    val names = files.map(_.getFileName.toString)
+    assert(names == List("something_1.json"))
+    val bytes = Files.readAllBytes(files.head)
+    val parsed = parse(new String(bytes))
+    assert(parsed.isRight)
+    val actualData = parsed.right.get.as[Class]
+    assert(actualData.contains(data))
+  }
+
+  it should "chose maximum number" in {
+    val data = Class("строка", 0, boolean = false)
+    val files = List(
+      "name_22.json",
+      "name_34.xml",
+      "name_18.json",
+      "names_40.json",
+      "name50.json",
+      "name_name.json"
+    )
+    for (file <- files)
+      Files.createFile(directory.resolve(file))
+    await(dataSaver.save("name", data))
+    val expected = "name_23.json" +: files
+    assert(getFileNames.toSet == expected.toSet)
+  }
+
+  def getFileNames: List[String] = {
+    val files = Files.list(directory).iterator().asScala.toList
+    files.map(_.getFileName.toString)
+  }
+
+  def await[T](future: Future[T]): T =
+    Await.result(future, Duration.Inf)
+}
