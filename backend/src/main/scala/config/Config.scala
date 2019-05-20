8762d91aa96f6af99ae1ff18e05edda415f800d5
imbar <imbrkvskh@gmail.com>
2019-05-20 21:36:38 +0500

    add phonebook saving directory to config

--- backend/src/main/scala/config/Config.scala
+++ backend/src/main/scala/config/Config.scala
@@ -1,11 +1,13 @@
 package config
 
-case class Config(server: ServerConfig, database: DatabaseConfig)
+case class Config(server: ServerConfig, database: DatabaseConfig, fileSystem: FileSystemConfig)
 
 case class ServerConfig(interface: String, port: Int)
 
 case class DatabaseConfig(url: String, database: String, backend: Credentials, migration: Option[Credentials]) {
   val databaseUrl = s"$url;databaseName=$database"
 }
+
+case class FileSystemConfig(storage: String)
 
 case class Credentials(user: String, password: String)
