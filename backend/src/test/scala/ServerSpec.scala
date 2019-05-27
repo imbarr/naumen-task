fb48cc94b3301055eee814503b6d4e39b7fed913
imbar <imbrkvskh@gmail.com>
2019-05-27 13:58:20 +0500

    refactor tests

--- backend/src/test/scala/ServerSpec.scala
+++ backend/src/test/scala/ServerSpec.scala
@@ -4,36 +4,28 @@ import akka.http.scaladsl.model.headers._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.typesafe.scalalogging.Logger
+import config.CacheConfig
 import data.Implicits._
 import data._
 import filesystem.DataSaver
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{Assertion, BeforeAndAfter, FlatSpec}
+import org.scalatest.{BeforeAndAfter, FlatSpec}
 import server.{Server, TaskManager}
 import storage.Book
 import util.CirceMarshalling._
 import util.TestData.{entriesWithIds, entry}
 import util.TestUtils._
 
+import scala.concurrent.duration.Duration
 import scala.concurrent.{ExecutionContext, Future}
 
 class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter with MockFactory {
   implicit val log = Logger("naumen-task-test")
   implicit val executionContext = ExecutionContext.global
 
-  var book: Book = _
-  var dataSaver: DataSaver = _
-  var taskManager: TaskManager = _
-  var server: Server = _
+  val cacheConfig = CacheConfig(10, 10, Duration.Inf, Duration.Inf)
 
-  before {
-    book = mock[Book]
-    dataSaver = mock[DataSaver]
-    taskManager = mock[TaskManager]
-    server = new Server(book, dataSaver, taskManager)
-  }
-
-  "Server" should "return all phonebook entries" in {
+  "Server" should "return all phonebook entries" in serverTest { (book, _, _, server) =>
     book.getEntries _ expects(None, None, None) returning Future.successful(entriesWithIds)
     Get("/phonebook") ~> server.route ~> check {
       assert(status == StatusCodes.OK)
@@ -42,7 +34,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "return range of entries" in {
+  it should "return range of entries" in serverTest { (book, _, _, server) =>
     val start = 11
     val end = 12
     val total = 100
@@ -58,7 +50,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "return entry by id" in {
+  it should "return entry by id" in serverTest { (book, _, _, server) =>
     val id = 22
     book.getById _ expects id returning Future.successful(Some(entry))
     Get(s"/phonebook/$id") ~> server.route ~> check {
@@ -68,7 +60,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "create new phonebook entry" in {
+  it should "create new phonebook entry" in serverTest { (book, _, _, server) =>
     val id = 122
     book.add _ expects entry returning Future.successful(Some(id))
     Post("/phonebook", entry) ~> server.route ~> check {
@@ -77,7 +69,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "find entries by name substring" in {
+  it should "find entries by name substring" in serverTest { (book, _, _, server) =>
     val substring = "Doe"
     book.getEntries _ expects(Some(substring), None, None) returning Future.successful(entriesWithIds)
 
@@ -89,7 +81,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "find entries by telephone substring" in {
+  it should "find entries by telephone substring" in serverTest { (book, _, _, server) =>
     val substring = "+7"
     book.getEntries _ expects(None, Some(substring), None) returning Future.successful(entriesWithIds)
 
@@ -101,7 +93,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "update entry" in {
+  it should "update entry" in serverTest { (book, _, _, server) =>
     val id = 11
     val name = "name"
     val phone = entry.phone
@@ -113,13 +105,10 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
       Patch(s"/phonebook/$id", NameWrapper(name)),
       Patch(s"/phonebook/$id", PhoneNumberWrapper(phone))
     )
-
-    testForAll(requests) {
-      assert(status == StatusCodes.OK)
-    }
+    assertStatusForAll(server, requests, StatusCodes.OK)
   }
 
-  it should "delete entry" in {
+  it should "delete entry" in serverTest { (book, _, _, server) =>
     val id = 11
     book.remove _ expects id returning Future.successful(true)
     Delete(s"/phonebook/$id") ~> server.route ~> check {
@@ -127,7 +116,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "save phonebook" in {
+  it should "save phonebook" in serverTest { (_, _, taskManager, server) =>
     val id = 22
     taskManager.add _ expects * returning Some(id)
     Post("/files") ~> server.route ~> check {
@@ -136,7 +125,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     }
   }
 
-  it should "not save phonebook if another task in progress" in {
+  it should "not save phonebook if another task in progress" in serverTest { (_, _, taskManager, server) =>
     taskManager.add _ expects * returning None
     Post("/files") ~> server.route ~> check {
       assert(status == StatusCodes.TooManyRequests)
@@ -156,7 +145,7 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
     taskStatusTest(taskResult, expectedStatus)
   }
 
-  it should "return 404 for non-existing resources" in {
+  it should "return 404 for non-existing resources" in serverTest { (book, _, taskManager, server) =>
     val id = 20
     val name = "name"
     val phone = entry.phone
@@ -171,27 +160,20 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
       Delete(s"/phonebook/$id"),
       Patch(s"/phonebook/$id", BookEntry(name, phone))
     )
-
-    testForAll(requests) {
-      assert(status == StatusCodes.NotFound)
-    }
+    assertStatusForAll(server, requests, StatusCodes.NotFound)
   }
 
-  it should "return 405 for not allowed methods" in {
-    val id = 22
+  it should "return 405 for not allowed methods" in serverTest { (_, _, _, server) =>
     val requests = List(
       Delete("/phonebook"),
       Patch("/phonebook"),
-      Put(s"/phonebook/$id"),
+      Put(s"/phonebook/22"),
       Get("/files")
     )
-
-    testForAll(requests) {
-      assert(status == StatusCodes.MethodNotAllowed)
-    }
+    assertStatusForAll(server, requests, StatusCodes.MethodNotAllowed)
   }
 
-  it should "return 400 for incorrect requests" in {
+  it should "return 400 for incorrect requests" in serverTest { (_, _, _, server) =>
     val requests = List(
       Post("/phonebook", HttpEntity(ContentTypes.`application/json`, "")),
       Post("/phonebook", NameWrapper("name")),
@@ -202,32 +184,70 @@ class ServerSpec extends FlatSpec with ScalatestRouteTest with BeforeAndAfter wi
       Get(Uri("/phonebook").withQuery(Query("start" -> "50"))),
       Post("/phonebook", Map("name" -> "John", "phone" -> "88005553535"))
     )
-
-    testForAll(requests) {
-      assert(status == StatusCodes.BadRequest)
-    }
+    assertStatusForAll(server, requests, StatusCodes.BadRequest)
   }
 
-  it should "return 409 for same entry added" in {
+  it should "return 409 for same entry added" in serverTest { (book, _, _, server) =>
     book.add _ expects entry returning Future.successful(None)
     Post("/phonebook", entry) ~> server.route ~> check {
       assert(status == StatusCodes.Conflict)
     }
   }
 
-  private def testForAll(requests: List[HttpRequest])(assertion: => Assertion): Unit =
-    for (request <- requests)
-      request ~> Route.seal(server.route) ~> check {
-        assertion
+  "Server (with caching)" should "cache responses for GET requests" in serverTestWithCaching { (book, _, _, server) =>
+    book.getEntries _ expects(None, None, None) returning Future.successful(entriesWithIds)
+    for (_ <- 1 to 3) {
+      Get("/phonebook") ~> server.route ~> check {
+        assert(status == StatusCodes.OK)
+        val result = responseAs[List[BookEntryWithId]]
+        assert(result == entriesWithIds)
       }
-
-  private def taskStatusTest(taskResult: Future[Unit], expectedStatus: TaskStatus): Unit = {
-    val id = 1
-    taskManager.get _ expects id returning Some(taskResult)
-    Get(s"/tasks/$id") ~> server.route ~> check {
-      assert(status == StatusCodes.OK)
-      val result = responseAs[TaskStatus]
-      assert(result == expectedStatus)
     }
   }
+
+  it should "not cache responses for another methods" in serverTestWithCaching { (book, _, _, server) =>
+    val id = 11
+    book.remove _ expects id returning Future.successful(true)
+    book.remove _ expects id returning Future.successful(false)
+    Delete(s"/phonebook/$id") ~> server.route ~> check {
+      assert(status == StatusCodes.OK)
+    }
+    Delete(s"/phonebook/$id") ~> Route.seal(server.route) ~> check {
+      assert(status == StatusCodes.NotFound)
+    }
+  }
+
+  private def serverTest(test: (Book, DataSaver, TaskManager, Server) => Unit): Unit =
+    serverTest(None, test)
+
+  private def serverTestWithCaching(test: (Book, DataSaver, TaskManager, Server) => Unit): Unit =
+    serverTest(Some(cacheConfig), test)
+
+  private def serverTest(cacheConfig: Option[CacheConfig],
+                         test: (Book, DataSaver, TaskManager, Server) => Unit): Unit = {
+    val book = mock[Book]
+    val dataSaver = mock[DataSaver]
+    val taskManager = mock[TaskManager]
+    val server = new Server(book, dataSaver, taskManager, cacheConfig)
+    test(book, dataSaver, taskManager, server)
+  }
+
+  private def assertStatusForAll(server: Server, requests: List[HttpRequest], statusCode: StatusCode): Unit =
+    for (request <- requests)
+      request ~> Route.seal(server.route) ~> check {
+        assert(status == statusCode)
+      }
+
+  private def taskStatusTest(taskResult: Future[Unit], expectedStatus: TaskStatus): Unit =
+    serverTest { (_, _, taskManager, server) =>
+      val id = 1
+      taskManager.get _ expects id returning Some(taskResult)
+      Get(s"/tasks/$id") ~> server.route ~> check {
+        assert(status == StatusCodes.OK)
+        val result = responseAs[TaskStatus]
+        assert(result == expectedStatus)
+      }
+    }
+
+
 }
